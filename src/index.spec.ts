bbbb278266ad42178f776ae0a44f5f09dbbca17a
SeqSEE <sarah@sqdmc.net>
2022-05-15 18:53:32 -0400

    add tests

---
+++ src/index.spec.ts
@@ -0,0 +1,39 @@
+import MNS, { APIProvider, getMNSAddress } from './';
+import Name from './mns/Name';
+import { equal } from 'assert';
+
+describe('Typescript usage suite', () => {
+  const network = 'TestNet';
+  const provider = new APIProvider(network);
+
+  const mns = new MNS(network, provider, getMNSAddress(network));
+
+  const name: Name = mns.name('first.mrx');
+
+  it('should return expected TestNet MRX address', async () => {
+    const address = await name.getAddress('MRX');
+    equal(address, 'maTQfd4w7mqCzGL32RgBFMYY9ehCmjLEGf');
+  });
+
+  it('should return expected TestNet owner', async () => {
+    const owner = await name.getOwner();
+    equal(
+      owner.toLowerCase().replace('0x', ''),
+      'c87bb8ab63de99a58a5339217c4a1c92f0fbfefe'
+    );
+  });
+
+  it('should return expected TestNet name given Ethereum checksum address', async () => {
+    const addrName = await mns.getName(
+      '0xC87bB8Ab63De99A58a5339217C4A1C92f0FBFEFe'
+    );
+    equal(addrName, 'Test non-owner');
+  });
+
+  it('should return expected TestNet name given hex address', async () => {
+    const addrName = await mns.getName(
+      'c87bb8ab63de99a58a5339217c4a1c92f0fbfefe'
+    );
+    equal(addrName, 'Test non-owner');
+  });
+});
