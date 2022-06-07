3539d10bde2a12e2ac8bb4d4c16be76513848223
SeqSEE <sarah@sqdmc.net>
2022-06-07 15:14:17 -0400

    change name

---
+++ src/mrx/interface/IERC721Enumerable.ts
@@ -0,0 +1,25 @@
+import IERC721 from './IERC721';
+
+/**
+ * Interface that represents a contract, which implements EIP721Enumerable
+ *
+ * @interface
+ */
+export default interface IERC721Enumerable extends IERC721 {
+  /**
+   * Returns the total amount of tokens stored by the contract.
+   */
+  totalSupply(): Promise<bigint>;
+
+  /**
+   * Returns a token ID owned by `owner` at a given `index` of its token list.
+   * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
+   */
+  tokenOfOwnerByIndex(owner: string, index: bigint): Promise<string>;
+
+  /**
+   * Returns a token ID at a given `index` of all the tokens stored by the contract.
+   * Use along with {totalSupply} to enumerate all tokens.
+   */
+  tokenByIndex(index: bigint): Promise<string>;
+}
